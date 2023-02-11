60a4d4894dd17d1a093170065f60ccc0790af742
Abdullakh Mursalov <a.k.mursalov@gmail.com>
2023-02-11 04:00:19 +0300

    fix ig url pattern

--- src/main.py
+++ src/main.py
@@ -145,7 +145,7 @@ if __name__ == '__main__':
     app.add_handler(
         InlineQueryHandler(
             instagram_inline_query_handler,
-            pattern=re.compile(r".*instagram\.com/reels.*"),
+            pattern=re.compile(r".*instagram\.com/reel.*"),
         )
     )
     app.add_handler(
