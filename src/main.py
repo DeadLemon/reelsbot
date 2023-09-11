e529da8482e8e171cf86357d196f5e4670e7682e
Abdullakh Mursalov <a.k.mursalov@gmail.com>
2023-09-11 06:26:27 +0300

    fix reels link

--- src/main.py
+++ src/main.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
     app.add_handler(
         InlineQueryHandler(
             instagram_inline_query_handler,
-            pattern=re.compile(r".*instagram\.com/reel.*"),
+            pattern=re.compile(r".*instagram\.com/(p|reels).*"),
         )
     )
     app.add_handler(
