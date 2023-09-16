8807ece941f097c8168b2d35d8e3857c61203fac
Abdullakh Mursalov <a.k.mursalov@gmail.com>
2023-09-16 21:46:08 +0300

    fix

--- src/instagram.py
+++ src/instagram.py
@@ -1,9 +1,12 @@
 import asyncio as aio
+import logging
 from typing import Optional
 
 import instagrapi as ig
 import instagrapi.exceptions as igexc
 import instagrapi.types as igtypes
+
+log = logging.getLogger(__name__)
 
 _semaphore = aio.Semaphore(1)
 
@@ -16,6 +19,7 @@ async def get_media_info_from_url(c: ig.Client, url: str) -> Optional[igtypes.Me
 def _get_media_info_from_url(c: ig.Client, url: str) -> Optional[igtypes.Media]:
     pk = c.media_pk_from_url(url)
     try:
-        return c.media_info(pk)
+        return c.media_info_v1(pk)
     except igexc.ClientError:
+        log.error("failed to get media info")
         return None
